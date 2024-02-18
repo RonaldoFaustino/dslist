589bb18aae7cdc43257cb4902e860ae7849a3b43
Ronaldo Faustino <ohnirucse@msn.com>
2024-02-18 13:57:54 -0300

    Move games on list

--- src/main/java/com/example/dslist/services/GameListService.java
+++ src/main/java/com/example/dslist/services/GameListService.java
@@ -11,6 +11,7 @@ import com.example.dslist.dto.GameListDTO;
 import com.example.dslist.dto.GameMinDTO;
 import com.example.dslist.entities.Game;
 import com.example.dslist.entities.GameList;
+import com.example.dslist.projections.GameMinProjection;
 import com.example.dslist.repositories.GameListRepository;
 import com.example.dslist.repositories.GameRepository;
 
@@ -19,6 +20,9 @@ public class GameListService {
 	
 	@Autowired
 	private GameListRepository gameListRepository;
+	
+	@Autowired
+	private GameRepository gameRepository;
 	
 	/**
 	 * A notation @transactional serve para o banco entender que sera somente escrita,
@@ -31,6 +35,23 @@ public class GameListService {
 	List<GameList> result = gameListRepository.findAll();
 	List<GameListDTO> dto = result.stream().map(x -> new GameListDTO(x)).toList();
 	return dto;
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for(int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+		
 	}
 	
 
